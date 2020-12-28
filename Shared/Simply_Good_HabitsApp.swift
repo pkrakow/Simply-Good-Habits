b61a0136faa776f0e786bb691abde6efd99cc570
pkrakow <pkrakow@gmail.com>
2020-12-28 15:52:06 -0800

    Initial Commit

---
+++ Shared/Simply_Good_HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simply_Good_HabitsApp.swift
+//  Shared
+//
+//  Created by Paul Krakow on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct Simply_Good_HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
