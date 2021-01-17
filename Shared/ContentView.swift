746f2c0b32a1f61512ba53ffbaa2b098d9f65510
Paul Krakow <pkrakow@gmail.com>
2021-01-17 12:03:15 -0800

    refactored to use CoreData instead of state variables - still have some clean-up to do

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,21 +11,26 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
+
+    // Retrieve stored habits from CoreData
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+        entity: Habit.entity(),
+        sortDescriptors: [
+            // Note that the sort order is in place to make it easy to grab the latest entry in CoreData
+            // Do not change this
+            NSSortDescriptor(keyPath: \Habit.creationDate, ascending: false)
+        ]
+    ) var habits: FetchedResults<Habit>
     
+    // UPDATE SOON: Get rid of these state values
     // Data for this view controller
-    @State var habitCount = 0
+    @State var habitCount: Int64 = 0
     @State var yesterdayHabitCount = 0
     @State var habitDictionary = [Date:Int]()
     @State var moreOrLess = true
-    @State var yesterdaysHabitCount = 0
-    let habitTarget = 5
+
     
-
-
+    // Upgrade to use iCloud to persist results across devices
     // After that, build out the logic to change the button color based on the previous target
     
     
@@ -35,16 +40,19 @@ struct ContentView: View {
                 .font(.largeTitle)
             Button(
                 action: { incrementHabitCount() },
-                label: { Text("\(habitCount)") }
+                //label: { Text("\(habitCount)") }
+                label: { Text("\((habits.first?.count ?? 0))") }
             )
             .buttonStyle(DynamicRoundButtonStyle())
             HStack {
                 Button(
-                    action: { moreOrLess = true },
+                    //action: { moreOrLess = true },
+                    action: { habits.first?.moreOrLess = true},
                     label: { Text("doMore") }
                 ).buttonStyle(DoMoreDoLessUndoButtonStyle(actionType: .doMore, moreOrLess: moreOrLess))
                 Button(
-                    action: { moreOrLess = false },
+                    //action: { moreOrLess = false },
+                    action: { habits.first?.moreOrLess = false},
                     label: { Text("doLess") }
                         )
                 .buttonStyle(DoMoreDoLessUndoButtonStyle(actionType: .doLess, moreOrLess: moreOrLess))
@@ -56,9 +64,49 @@ struct ContentView: View {
                 .buttonStyle(DoMoreDoLessUndoButtonStyle(actionType: .undo))
             }.padding()
         }
+        
+        // When the app loads
+        .onAppear() {
+            //print(habits)
+            //print(habits.count)
+            //print(habits.isEmpty)
+            
+            // Check if this is the first time the app has been used
+            if habits.count == 0
+            {
+                //print("First Use")
+                // Create the first Habit entity in CoreData
+                // UPDATE LATER: Replace this code with code that navigates to a view to setup the first habit
+                addHabit(uuid: UUID(), creationDate: Date(), name: "placeholder", moreOrLess: moreOrLess, target: 0, count: 0)
+                saveContext()
+                //addHabit(uuid: UUID(), creationDate: Date(), name: "placeholder1", moreOrLess: moreOrLess, target: 1, count: 1)
+            }
+            //print(habits.first?.name)
+            
+            // Check if it is a new day, and if so, setup the app for the new day
+            if newDayDetector() {
+                startNewDay()
+            }
+            
+        }
+        
+        // When the app moves to the foreground
+        .onReceive(NotificationCenter.default.publisher(for:
+            UIApplication.willEnterForegroundNotification)) { _ in
+            //print(habits)
+            //print(habits.count)
+            //print(habits.isEmpty)
+            
+            // Check if it is a new day, and if so, setup the app for the new day
+            if newDayDetector() {
+                startNewDay()
+            }
+            
+        }
+        
     }
     
-    
+    // UPDATE LATER: need to get rid of the habitDictionary and do this lookup on the CoreData objects
     func newDayDetector() -> Bool {
         // Lookup what year, month and day it is right now
         let formatter = DateFormatter()
@@ -66,8 +114,9 @@ struct ContentView: View {
         let today = formatter.string(from: Date())
         
         // Lookup the latest entry in the habitDictionary
-        let latestHabitDictionaryEntry = habitDictionary.max { a, b in a.key < b.key }
-        let dayOfLastEntry = formatter.string(from: latestHabitDictionaryEntry?.key ?? Date())
+        //let latestHabitDictionaryEntry = habitDictionary.max { a, b in a.key < b.key }
+        //let dayOfLastEntry = formatter.string(from: latestHabitDictionaryEntry?.key ?? Date())
+        let dayOfLastEntry = formatter.string(from: habits.first?.creationDate ?? Date())
         
         // Check if it was from a day other than today
         if dayOfLastEntry != today {
@@ -77,79 +126,68 @@ struct ContentView: View {
         }
     }
     
+    // Prep the app for a new day of use
+    func startNewDay() -> Void {
+        // Create a new habit entry that takes the persistent elements of the last entry, and updates the creationDate and count for the new day
+        addHabit(uuid: (habits.first?.uuid)!, creationDate: Date(), name: (habits.first?.name)!, moreOrLess: (habits.first?.moreOrLess)!, target: (habits.first?.target)!, count: 0)
+        saveContext()
+    }
+    
+    // Increase the habit count by 1
     func incrementHabitCount() -> Void {
-        
-        // Check if it was from a day other than today
-        if newDayDetector() {
-            yesterdaysHabitCount = habitCount
-            habitCount = 1
-        } else {
-            habitCount += 1
-        }
-        habitDictionary[Date()] = 1
-        //print("habitCount \(habitCount)")
-        //print("habitDictionary: \(habitDictionary)")
+        habits.first?.count += 1
+        saveContext()
     }
     
+    // Decrease the habit count by 1
     func decrementHabitCount() -> Void {
+        if habits.first?.count ?? 0 > 0 {
+            habits.first?.count -= 1
+            saveContext()
+        }
+    }
+    
+    // Save habits to CoreData
+    func saveContext() {
+      do {
+        try viewContext.save()
+      } catch {
+        print("Error saving managed object context: \(error)")
+      }
+    }
+    
+    // Add a new habit
+    func addHabit(uuid: UUID, creationDate: Date, name: String, moreOrLess: Bool,  target: Int64, count: Int64) {
+        // Create a newHabit object
+        let newHabit = Habit(context: viewContext)
         
-        // Check if it was from a day other than today
-        if newDayDetector() {
-            yesterdaysHabitCount = habitCount
-            habitCount = 0
-        } else {
-            if habitCount > 0 {
-                habitCount -= 1
-                habitDictionary[Date()] = -1
-            } else {
-                habitCount = 0
-            }
-        }
-
-        //print("habitCount \(habitCount)")
-        //print("habitDictionary: \(habitDictionary)")
+        // Populate the attributes of the newHabit
+        newHabit.uuid = UUID()
+        newHabit.creationDate = Date()
+        newHabit.name = name
+        newHabit.moreOrLess = moreOrLess
+        newHabit.target = target
+        newHabit.count = count
+        
+        // Save all of the habits including the new one
+        saveContext()
     }
     
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+    // Delete a habit
+    func deleteHabit(at offsets: IndexSet) {
+        // Go through the CoreData index to find and delete the specific habit
+        offsets.forEach { index in
+            let habit = self.habits[index]
+            self.viewContext.delete(habit)
+      }
+      
+        // Save the updated list of habits to CoreData
+      saveContext()
     }
     
+
 }
 
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 
 /*
